ffe113c27f7ef920f33a64dfe3923b650825264d
InteractiveTechHub <techprohub2025@gmail.com>
2025-03-07 20:55:38 -0300

    added more one cors

--- Services/LeadsHub/LeadsHub.Api/Program.cs
+++ Services/LeadsHub/LeadsHub.Api/Program.cs
@@ -31,7 +31,7 @@ SD.WhatAppApiUrl = configuration.GetValue<string>("ApiUrl:WhatsAppApi") ?? strin
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
-        builder.WithOrigins("http://localhost:4200", "http://localhost", "http://http://93.127.215.157")
+        builder.WithOrigins("http://localhost:4200", "http://localhost", "http://http://93.127.215.157", "http://jsdesenvolvimento.shop/")
                .AllowAnyMethod()
                .AllowAnyHeader()
                .AllowCredentials());
